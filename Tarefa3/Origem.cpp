f2eecac1c9997d49a1a3fe7a0436517180a7b9e6
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-02-25 15:45:24 -0300

    Add files via upload

---
+++ Tarefa3/Origem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "EU" << std::endl;
+	std::cout << "VOU" << std::endl;
+	std::cout << "APRENDER" << std::endl;
+	std::cout << "C++" << std::endl;
+	
+	system("PAUSE");
+	return 0;
+}
