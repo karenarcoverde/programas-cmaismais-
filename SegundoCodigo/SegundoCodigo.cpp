07ec35efc776e10d6f454de27a2d87e1e429787f
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-02-25 15:44:14 -0300

    Add files via upload

---
+++ SegundoCodigo/SegundoCodigo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Segundo Programa" << std::endl;
+
+	system ("color 5F");
+	system("PAUSE");
+	
+	return 0;
+	// CTRL  + SHIFT + B - COMPILAR
+	// CTRL + F5 - EXECUTA O PROGRAMA
+}
