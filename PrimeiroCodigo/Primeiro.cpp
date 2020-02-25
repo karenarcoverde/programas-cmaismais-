ee6d2e42d771311f84c98b973a3191038d6475be
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-02-25 15:43:32 -0300

    Add files via upload

---
+++ PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Nome: Karen" << std::endl;
+	std::cout << "Idade: 24 anos" << std::endl;
+	std::cout << "O caminho para o sucesso eh persistir" << std::endl;
+	system("PAUSE");
+	return 0;
+}
