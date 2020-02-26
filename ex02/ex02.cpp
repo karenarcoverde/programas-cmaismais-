c2c24cce175ebda5e1b74598bb7fc03221d0024e
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-02-25 21:38:22 -0300

    Add files via upload

---
+++ ex02/ex02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/* Altere o programa do exercício 01) para que cada palavra da frase 
+Vou aprender a programar na Linguagem C++ apareça em linhas diferentes */
+int main()
+{
+
+	std::cout << "Vou" << "\n"
+		<< "Aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem C++" << "\n";
+		
+	system("PAUSE");
+	return 0;
+}
