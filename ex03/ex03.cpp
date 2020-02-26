4408e4ce7e4d711b411fad3ea78782548d99ecf5
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-02-25 21:39:10 -0300

    Add files via upload

---
+++ ex03/ex03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// Aluna: Karen Arcoverde
+// Data: 25/02/2020
+/* O programa a seguir coloca na tela a frase: Vou aprender a 
+programa na Linguagem C++. Contudo cada palavra será exibida em 
+uma única linha através da inserção de quebra de linhas */
+
+int main()
+{
+
+	std::cout << "Vou" << "\n"
+		<< "Aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem C++" << "\n";
+
+	system("PAUSE");
+	return 0;
+}
+
